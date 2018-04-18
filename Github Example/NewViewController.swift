cc1726eed180b302f428c06cbcffce26a299434a
Danielle J. Drelicharz <ddrelicharz9099@jh-mbas-ddrelicharz9099.d214.org>
2018-04-18 15:02:46 -0500

    added vc newViewController

---
+++ Github Example/NewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewViewController.swift
+//  Github Example
+//
+//  Created by Danielle J. Drelicharz on 4/18/18.
+//  Copyright © 2018 Danielle J. Drelicharz. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
