54760820108324229ff448bd0a646c6d1cb4b9fb
Danielle J. Drelicharz <ddrelicharz9099@jh-mbas-ddrelicharz9099.d214.org>
2018-04-18 14:43:50 -0500

    Initial Commit

---
+++ Github Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github Example
+//
+//  Created by Danielle J. Drelicharz on 4/18/18.
+//  Copyright © 2018 Danielle J. Drelicharz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
